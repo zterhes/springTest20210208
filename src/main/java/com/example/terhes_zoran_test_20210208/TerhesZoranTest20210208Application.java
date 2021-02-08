fa2d6776b8fdafcca57e6494c732143a40eadf83
zterhes <zterhes1@gmail.com>
2021-02-08 10:41:24 +0100

    Init commit before test

---
+++ src/main/java/com/example/terhes_zoran_test_20210208/TerhesZoranTest20210208Application.java
@@ -0,0 +1,13 @@
+package com.example.terhes_zoran_test_20210208;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TerhesZoranTest20210208Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TerhesZoranTest20210208Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/terhes_zoran_test_20210208/TerhesZoranTest20210208ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.terhes_zoran_test_20210208;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TerhesZoranTest20210208ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
